94b7a03c265bcb123870df61e7d17c2b67fd911d
bozar <5583771+Bozar@users.noreply.github.com>
2024-01-12 15:36:36 +0800

    Let MainRoot connect signals based on a pre-defined dictionary.

---
+++ library/signal_connection_data.gd
@@ -0,0 +1,18 @@
+class_name SignalConnectionData
+
+
+const INIT_WORLD: String = "InitWorld"
+const SPRITE_ROOT: String = "SpriteRoot"
+
+
+const SIGNAL_SPRITES_CREATED: String = "sprites_created"
+
+
+# {source_node: {signal_name: [target_node_1, ...]}, ...}
+const SIGNAL_CONNECTIONS: Dictionary = {
+    INIT_WORLD: {
+        SIGNAL_SPRITES_CREATED: [
+            SPRITE_ROOT,
+        ],
+    },
+}
--- scene/main/main_root.gd
+++ scene/main/main_root.gd
@@ -3,9 +3,30 @@ extends Node2D
 
 
 func _ready() -> void:
-    @warning_ignore("unsafe_method_access")
-    get_node("%InitWorld")["sprites_created"].connect(
-            get_node("%SpriteRoot")["_on_InitWorld_sprites_created"])
+    _connect_signals(SignalConnectionData.SIGNAL_CONNECTIONS)
 
     (%InitWorld as InitWorld).create_world()
     (%PlayerInput as PlayerInput).set_process_unhandled_input(true)
+
+
+func _connect_signals(signal_connections: Dictionary) -> void:
+    var signals_from_one_node: Dictionary
+    var target_nodes: Array
+    var source_signal: Signal
+    var target_function: Callable
+
+    for source_node: String in signal_connections.keys():
+        signals_from_one_node = signal_connections[source_node]
+
+        for signal_name: String in signals_from_one_node.keys():
+            target_nodes = signals_from_one_node[signal_name]
+
+            for target_node: String in target_nodes:
+                source_signal = get_node("%" + source_node)[signal_name]
+                target_function = get_node("%" + target_node)[
+                        "_on_" + source_node + "_" + signal_name]
+
+                if source_signal.connect(target_function) == \
+                        ERR_INVALID_PARAMETER:
+                    print("Signal error: %s -> %s, %s." %
+                            [source_node, target_node, signal_name])
