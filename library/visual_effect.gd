087b66017eb4cf0e4e4f9c9447f244749f593920
bozar <5583771+Bozar@users.noreply.github.com>
2024-01-18 22:50:43 +0800

    Add: VisualEffect.switch_sprite().

--- library/visual_effect.gd
+++ library/visual_effect.gd
@@ -29,3 +29,16 @@ static func set_indicator_color() -> void:
 
     for i: Sprite2D in SearchHelper.get_sprites_by_tag(main_tag, ""):
         i.modulate = Palette.get_color(palette, main_tag, true)
+
+
+static func switch_sprite(sprite: Sprite2D, visual_tag: StringName) -> void:
+    var has_switched: bool = false
+
+    for i: Sprite2D in sprite.get_children():
+        if i.name == visual_tag:
+            i.visible = true
+            has_switched = true
+        else:
+            i.visible = false
+    if not has_switched:
+        push_error("Invalid visual tag: %s, %s." % [sprite, visual_tag])
