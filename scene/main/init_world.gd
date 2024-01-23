1a9967188c093c5c260b335ff4c0970ddeb5d282
bozar <5583771+Bozar@users.noreply.github.com>
2024-01-23 14:45:51 +0800

    Create a testing dungeon from text file.

--- scene/main/init_world.gd
+++ scene/main/init_world.gd
@@ -4,6 +4,12 @@ extends Node2D
 
 const INDICATOR_OFFSET: int = 32
 
+const PATH_TO_PREFAB: StringName = "resource/dungeon_prefab/dungeon.txt"
+
+const WALL_CHAR: StringName = "#"
+const TRAP_CHAR: StringName = "?"
+const GRUNT_CHAR: StringName = "G"
+
 
 func create_world() -> void:
     var tagged_sprites: Array[TaggedSprite] = []
@@ -11,9 +17,7 @@ func create_world() -> void:
 
     pc_coord = _create_pc(tagged_sprites)
     _create_floor(tagged_sprites)
-    _create_building(tagged_sprites)
-    _create_trap(tagged_sprites)
-    _create_actor(tagged_sprites)
+    _create_from_file(tagged_sprites)
     _create_indicator(pc_coord, tagged_sprites)
 
     SpriteFactory.create_sprites(tagged_sprites)
@@ -23,6 +27,28 @@ func _create_pc(tagged_sprites: Array[TaggedSprite]) -> Vector2i:
     var pc_coord: Vector2i = Vector2i(0, 0)
     tagged_sprites.push_back(CreateSprite.create_actor(SubTag.PC, pc_coord))
     return pc_coord
+
+
+func _create_from_file(tagged_sprites: Array[TaggedSprite]) -> void:
+    var parsed: ParsedFile = FileIo.read_as_line(PATH_TO_PREFAB)
+    if not parsed.parse_success:
+        return
+
+    var row: String
+
+    for y: int in parsed.output_line.keys():
+        row = parsed.output_line[y]
+        for x: int in range(0, row.length()):
+            match row[x]:
+                WALL_CHAR:
+                    tagged_sprites.push_back(CreateSprite.create_building(
+                            SubTag.WALL, Vector2i(x, y)))
+                GRUNT_CHAR:
+                    tagged_sprites.push_back(CreateSprite.create_actor(
+                            SubTag.GRUNT, Vector2i(x, y)))
+                TRAP_CHAR:
+                    tagged_sprites.push_back(CreateSprite.create_trap(
+                            SubTag.AMMO, Vector2i(x, y)))
 
 
 func _create_floor(tagged_sprites: Array[TaggedSprite]) -> void:
@@ -54,25 +80,3 @@ func _create_indicator(coord: Vector2i, tagged_sprites: Array[TaggedSprite]) \
         new_offset = indicators[i][1]
         tagged_sprites.push_back(CreateSprite.create(MainTag.INDICATOR,
                 i, new_coord, new_offset))
-
-
-func _create_building(tagged_sprites: Array[TaggedSprite]) -> void:
-    tagged_sprites.push_back(CreateSprite.create_building(SubTag.WALL,
-            Vector2i(2, 2)))
-
-
-func _create_trap(tagged_sprites: Array[TaggedSprite]) -> void:
-    tagged_sprites.push_back(CreateSprite.create_trap(SubTag.AMMO,
-            Vector2i(2, 4)))
-
-
-func _create_actor(tagged_sprites: Array[TaggedSprite]) -> void:
-    var coords: Array[Vector2i] = [
-        Vector2i(4, 2),
-        Vector2i(3, 2),
-        Vector2i(0, 2),
-        Vector2i(2, 1),
-    ]
-
-    for i: Vector2i in coords:
-        tagged_sprites.push_back(CreateSprite.create_actor(SubTag.GRUNT, i))
