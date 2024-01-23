531d4c3e4d23b4eb36f885b70fc8c68ca4607db8
bozar <5583771+Bozar@users.noreply.github.com>
2024-01-23 12:04:23 +0800

    Add helper functions to read files.

--- library/array_helper.gd
+++ library/array_helper.gd
@@ -10,3 +10,12 @@ static func filter(source: Array, is_valid: Callable, is_valid_args: Array) \
         if is_valid.call(i, is_valid_args):
             target.push_back(i)
     return target
+
+
+# modify(element_in_source: Variant, modify_args: Array) -> Variant
+static func map(source: Array, modify: Callable, modify_args: Array) -> Array:
+    var target: Array = []
+
+    for i: Variant in source:
+        target.push_back(modify.call(i, modify_args))
+    return target
