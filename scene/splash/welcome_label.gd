d4483b8bc8857c65498e5fc8766777d611195ebd
bozar <5583771+Bozar@users.noreply.github.com>
2024-04-02 15:33:20 +0800

    Add text to splash screen.

--- scene/splash/welcome_label.gd
+++ scene/splash/welcome_label.gd
@@ -2,6 +2,15 @@ class_name WelcomeLabel
 extends CustomLabel
 
 
+const PATH_TO_TEXT: String = "res://user/doc/splash_screen.md"
+const FAIL_SAFE_TEXT: String = "Press Space to continue."
+
+
 func init_gui() -> void:
+    var parsed_file: ParsedFile = FileIo.read_as_text(PATH_TO_TEXT)
+
     _set_font_color(true)
-    text = "Press Space to continue."
+    if parsed_file.parse_success:
+        text = parsed_file.output_text
+    else:
+        text = FAIL_SAFE_TEXT
---
+++ user/doc/splash_screen.md
@@ -0,0 +1,4 @@
+You are surrounded by a pack of giant growling wolves. Kill nine beasts to impress the ever watching Raven, and get yourself out of trouble. Remember: only your railgun can shoot them.
+
+Press Space to continue.
+
